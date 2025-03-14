ee996d2daf78f81283411de7f357b4e4b564d2dd
s27371 <27371@pjwstk.edu.pl>
2025-03-14 21:26:58 +0100

    Dodałem FindAverage

--- zad1/zad1/Program.cs
+++ zad1/zad1/Program.cs
@@ -1,4 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        int[] numbers = { 1, 2, 4 };
+        Console.WriteLine(FindAverage(numbers));
+    }
+    public static double FindAverage(int[] numbers)
+    {
+        double sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+        return sum/numbers.Length;
+
+    }
+}
